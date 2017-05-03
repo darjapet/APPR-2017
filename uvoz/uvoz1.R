8d1986d70066ead83255e3ea52a21de772262aa2
Darja Petrič <darjapetric1@gmail.com>
2017-05-03 08:50:07 +0200

    za uvoz

--- uvoz/uvoz1.R
+++ uvoz/uvoz1.R
@@ -1,11 +1,11 @@
 link <- "https://en.wikipedia.org/wiki/World_Tourism_rankings"
 stran <- html_session(link) %>% read_html()
-tabela <- stran %>% html_nodes(xpath="//table[@class='wikitable sortable']") %>%
+tabela1 <- stran %>% html_nodes(xpath="//table[@class='wikitable sortable']") %>%
   .[[6]] %>% html_table(dec = ".")
-summary(tabela)
+summary(tabela1)
 
-colnames(tabela) <- c("stevnik", "destinacija", "mednarodni turisti 2015", "mednarodni turisti 2014", "sprememba 2013-2014",
+colnames(tabela1) <- c("stevnik", "destinacija", "mednarodni turisti 2015", "mednarodni turisti 2014", "sprememba 2013-2014",
                       "sprememba 2014-2015")
 sl <- locale("sl", decimal_mark = ".")
 
-View(tabela)
+View(tabela1)
