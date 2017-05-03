28075e14f66835f7f025bcdeb3c7c1deb4399421
Darja Petrič <darjapetric1@gmail.com>
2017-05-03 09:48:28 +0200

    sprememba tabel

--- uvoz/uvoz1.R
+++ uvoz/uvoz1.R
@@ -7,5 +7,25 @@ summary(tabela1)
 colnames(tabela1) <- c("stevnik", "destinacija", "mednarodni turisti 2015", "mednarodni turisti 2014", "sprememba 2013-2014",
                       "sprememba 2014-2015")
 sl <- locale("sl", decimal_mark = ".")
+
+tabela1$`mednarodni turisti 2015` <- (tabela1$`mednarodni turisti 2015` %>% parse_number()) * 1000000
+tabela1$`mednarodni turisti 2014` <- (tabela1$`mednarodni turisti 2014` %>% parse_number()) * 1000000
+
 summary(tabela1)
-View(tabela1)
+#View(tabela1)
+
+
+tabela2 <- st_nocitev
+colnames(tabela2) <- c("leto", "drzava", "domacini/tujci", "vrednost", "vrsta nocitve",
+                       "stevilo gostov")
+summary(tabela2)
+#View(tabela2)
+
+tabela3 <- zaposljenost_v_turizmu
+colnames(tabela3) <- c("leto", "drzava", "panoga turizma", "starost delavcev", "vrsta zaposlitve", "tisoc",
+                       "stevilo zaposlenih")
+tabela3$`stevilo zaposlenih` <- (tabela3$`stevilo zaposlenih`) * 1000
+
+summary(tabela3)
+#View(tabela3)
+
