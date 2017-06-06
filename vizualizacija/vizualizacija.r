724f88e0bb954ec29e2c932437f4b24ae64216e3
darjapet <darjapetric1@gmail.com>
2017-06-06 22:07:24 +0200

    posodobitev prikaza grafov

--- vizualizacija/vizualizacija.r
+++ vizualizacija/vizualizacija.r
@@ -50,14 +50,8 @@ z <- ggplot() + geom_polygon(data = left_join(evropa, nocitve, by = c("name_sort
 #prikaz zaposljenih v turizmu v 10 najbolj obiskanih državah v letih - črtni
 
 g2 = ggplot(tabela3 %>% filter(drzava %in% c("France", "Italy", "Spain", "Germany", "Austria", "United Kingdom", "Netherlands","Croatia","Greece","Poland")) %>% 
-<<<<<<< HEAD
               group_by(leto, drzava) %>%
               summarise(zaposleni = sum(stevilo_zaposlenih, na.rm = TRUE)),
-=======
-              #filter(stevilo_zaposlenih != is.na()) %>%
-              group_by(leto, drzava) %>%
-              summarise(zaposleni = sum(stevilo_zaposlenih)) %>%
->>>>>>> b2a6b921de71f2edb0b169ec7c744dd136a39b81
            aes(x = leto, y = zaposleni, color = drzava)) + geom_line()
 
 
