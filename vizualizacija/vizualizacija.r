9cd2774fac3ad309403d46f1648e8dbedd2a9d6a
darjapet <darjapetric1@gmail.com>
2017-07-09 09:17:33 +0200

    dopolnitev vizualizacije

--- vizualizacija/vizualizacija.r
+++ vizualizacija/vizualizacija.r
@@ -18,7 +18,7 @@ evropa <- uvozi.zemljevid("http://www.naturalearthdata.com/http//www.naturaleart
 #prikaz praznega zemljevida
 
 ggplot() + geom_polygon(data = evropa, aes(x = long, y = lat, group = group)) + 
-  coord_map(xlim = c(-25, 45), ylim = c(32, 72))
+  coord_map(xlim = c(-25, 45), ylim = c(32, 72)) + theme(axis.title.x = element_blank(), axis.title.y = element_blank())
 
 
 #-------------------------------------------------------------------
@@ -49,10 +49,25 @@ zemljevid <- ggplot() + geom_polygon(data = left_join(evropa, nocitve, by = c("n
 
 #prikaz zaposljenih v turizmu v 10 najbolj obiskanih državah v letih - črtni
 
+slovar2 <- c(
+  "France" = "Francija", 
+  "Italy" = "Italija", 
+  "Spain" = "Španija", 
+  "Germany" = "Nemčija", 
+  "Austria" = "Avstrija", 
+  "United Kingdom" = "Velika Brtanija", 
+  "Netherlands" = "Nizozemska",
+  "Croatia" = "Hrvaška",
+  "Greece" = "Grčija",
+  "Poland" = "Poljska",
+  "drzava" = "Država"
+)
+
 g2 = ggplot(tabela3 %>% filter(drzava %in% c("France", "Italy", "Spain", "Germany", "Austria", "United Kingdom", "Netherlands","Croatia","Greece","Poland")) %>% 
               group_by(leto, drzava) %>%
               summarise(zaposleni = sum(stevilo_zaposlenih, na.rm = TRUE)),
-           aes(x = leto, y = zaposleni, color = drzava)) + geom_line()
+           aes(x = leto, y = zaposleni, color = slovar2[drzava])) + geom_line() +
+          guides(fill = guide_colourbar(title = "Država"))
 
 
 #-------------------------------------------------------------------
@@ -60,21 +75,36 @@ g2 = ggplot(tabela3 %>% filter(drzava %in% c("France", "Italy", "Spain", "German
 
 #število zaposljenih po starostnih skupinah skozi leta - črtni
 
+slovar3 <- c(
+  "From 15 to 24 years" = "15-24 let",
+  "From 25 to 34 years" = "25-34 let",
+  "From 35 to 44 years" = "35-44 let",
+  "From 45 to 54 years" = "45-54 let"
+)
+
 g3 = ggplot(tabela3 %>% 
               group_by(leto, starost_delavcev) %>%
               summarise(zaposleni = sum(stevilo_zaposlenih, na.rm = TRUE)),
-            aes(x = leto, y = zaposleni, color = starost_delavcev)) + geom_line()
+            aes(x = leto, y = zaposleni, color = slovar3[starost_delavcev])) + geom_line()
 
 
 #-------------------------------------------------------------------
 
 
 # število zaposljenih v evropi v posamezni panogi turizma v letu 2015 - stolpični
+
+slovar4 <- c(
+  "Accommodation" = "Nastanitev",
+  "Accommodation and food service activities" = "Hrana in nastanitev",
+  "Air transport" = "Zračni prevoz",
+  "Travel agency, tour operator reservation service and related activities" = "Turistične storitve"
+)
+
 g4 = ggplot(tabela3 %>% filter(leto %in% c("2015"))%>%
                group_by(panoga_turizma) %>%
                summarise(zaposleni = sum(stevilo_zaposlenih, na.rm = TRUE)),
-             aes(x = panoga_turizma, y = zaposleni)) + geom_col() +
-  theme(axis.text.x = element_text(angle = 30, hjust = 1))
+             aes(x = slovar4[panoga_turizma], y = zaposleni)) + geom_col() +
+  theme(axis.text.x = element_text(angle = 0, hjust = 0.5))
 
     
 #-------------------------------------------------------------------
@@ -82,10 +112,18 @@ g4 = ggplot(tabela3 %>% filter(leto %in% c("2015"))%>%
 
 #število nstanitev v različnih območjih po evropi - primerjava domačini in tujci - stolpični
 
+slovar5 <- c(
+  "Camping grounds, recreational vehicle parks and trailer parks" = "Kampiranje",
+  "Holiday and other short-stay accommodation" = "Počitnice",
+  "Hotels and similar accommodation" = "Hoteli",
+  "Total nights spent by non-residents" = "Tujci",
+  "Total nights spent by residents" = "Domačini")
+
 domacini_tujci <- tabela2 %>%
   filter(vrsta_nocitve %in% c("Hotels and similar accommodation", "Holiday and other short-stay accommodation", "Camping grounds, recreational vehicle parks and trailer parks"))%>%
 group_by(vrsta_nocitve, domacini_tujci) %>%
   summarise(gosti = sum(stevilo_gostov, na.rm = TRUE))
 
-g5 = ggplot(domacini_tujci, aes(x = vrsta_nocitve, y = gosti / 1e9, fill = domacini_tujci)) + geom_bar(stat = "identity", position = "dodge") +
-  theme(axis.text.x = element_text(angle = 45, hjust = 1)) + labs(x="vrsta nočitve", y="število gostov")
+g5 = ggplot(domacini_tujci, aes(x = slovar5[vrsta_nocitve], y = gosti / 1e9, fill = slovar5[domacini_tujci])) + geom_bar(stat = "identity", position = "dodge") +
+  theme(axis.text.x = element_text(angle = 0, hjust = 0.5)) + labs(x="Vrsta nočitve", y="število gostov") + 
+  guides(fill = guide_legend(title = "Tip gostov"))
